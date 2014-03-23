8c08261585521faf7dfa6d1a92b2aefed84a0645
El-Nath <gamakireta@gmail.com>
2014-03-23 16:04:29 +0700

    Initial Fastcharge adaption from faux's Mako

--- drivers/video/msm/mipi_orise_video_720p_pt.c
+++ drivers/video/msm/mipi_orise_video_720p_pt.c
@@ -14,20 +14,8 @@
 #include "msm_fb.h"
 #include "mipi_dsi.h"
 #include "mipi_orise.h"
-#include <linux/gpio.h>
-
-//Neal
-#define LCD_DIS_GPIO  70
-
-static bool dis = 1;
 
 static struct msm_panel_info pinfo;
-
-bool get_panel_info(void)
-{
-
-	return dis;
-}
 
 static struct mipi_dsi_phy_ctrl dsi_video_mode_phy_db_1080p = { 
 	/* 1920*1200, RGB888, 4 Lane 60 fps video mode */ 
@@ -49,22 +37,9 @@ static struct mipi_dsi_phy_ctrl dsi_video_mode_phy_db_1080p = {
 static int __init mipi_video_orise_720p_pt_init(void)
 {
 	int ret;
-	int rc = 0;
+
 	if (msm_fb_detect_client("mipi_video_orise_720p"))
 		return 0;
-/*OPPO Neal add for sharp panel*/
-	 rc = gpio_request(LCD_DIS_GPIO, "LCD_DIS_GPIO");
-        if (rc < 0)
-        {
-            pr_err("MIPI GPIO LCD_TE_GPIO request failed: %d\n", rc);
-            return -ENODEV;
-        }
-	 gpio_direction_output(LCD_DIS_GPIO,0);
-	
-	 dis = gpio_get_value(LCD_DIS_GPIO);
-	 printk(KERN_ERR "mipi_orise_lcd_probe Neal ******************************: dis = %d\n", dis);
-	 gpio_free(LCD_DIS_GPIO);
-/*OPPO Neal add end*/
 
 	pinfo.xres = 1080;
 	pinfo.yres = 1920;
@@ -77,28 +52,12 @@ static int __init mipi_video_orise_720p_pt_init(void)
 	pinfo.bpp = 24;
 #if 1		
 	/* OPPO 2013-03-07 Gousj Modify begin for solve the issue of lack of virtical pixel. */	
-		//Gousj modified  h_back_porch  from 100 to 101 	
-	/*OPPO Neal add for sharp panel*/
-
-	if(get_panel_info() == 0)
-		{
-			pinfo.lcdc.h_back_porch = 100;//100;//80;
-			pinfo.lcdc.v_back_porch = 5;	//must > 4,Otherwise,it will increase the burden of clock	huyu
-
-			printk(KERN_ERR "%s: register sharp device!\n", __func__);
-		}
-	else
-		{
-			pinfo.lcdc.h_back_porch = 101;//100;//80;
-			pinfo.lcdc.v_back_porch = 4;	//must > 4,Otherwise,it will increase the burden of clock	huyu
-			printk(KERN_ERR "%s: register jdi device!\n", __func__);
-		}	
-	/*OPPO Neal add end*/
-
+		//Gousj modified  h_back_porch  from 100 to 101 		
+		pinfo.lcdc.h_back_porch = 101;//100;//80;		
 		pinfo.lcdc.h_front_porch = 130;//120		
 		pinfo.lcdc.h_pulse_width = 8;		
 		//Modified by Gousj on date 2013-3-4 ,the value of v_back_porch decreased from 5 to 4 .		
-				
+		pinfo.lcdc.v_back_porch = 4;	//must > 4,Otherwise,it will increase the burden of clock	huyu		
 		pinfo.lcdc.v_front_porch = 3;		
 		//Modified by Gousj on date 2013-3-4 ,the value of v_pulse_width decreased from 2 to 1 .		
 		pinfo.lcdc.v_pulse_width = 1;//2;		
@@ -119,11 +78,7 @@ static int __init mipi_video_orise_720p_pt_init(void)
 	pinfo.mipi.hbp_power_stop = FALSE;
 	pinfo.mipi.hsa_power_stop = FALSE;
 	pinfo.mipi.eof_bllp_power_stop = FALSE;
-/* OPPO 2013-10-10 gousj Add begin for sent mipi cmd in HS mode */
-#ifdef CONFIG_VENDOR_EDIT
-	pinfo.mipi.bllp_power_stop = TRUE;
-#endif
-/* OPPO 2013-10-10 gousj Add end */
+	pinfo.mipi.bllp_power_stop = FALSE;
 	pinfo.mipi.traffic_mode = DSI_BURST_MODE;
 	pinfo.mipi.dst_format = DSI_VIDEO_DST_FORMAT_RGB888;
 	pinfo.mipi.vc = 0;
